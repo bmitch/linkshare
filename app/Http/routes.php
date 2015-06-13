0eb3986e49b8aa1a514e38cb41b1f0ecfcc26262
Bill Mitchell <wkmitch@gmail.com>
2015-06-13 08:14:37 +0000

    Added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,50 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+// We use the Laravel built in Auth
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
+//----------------------------------------//
 
-Route::get('/', function () {
-    return view('welcome');
+// Display users, subs and front page
+Route::get('/', 'PostController@frontpage');
+Route::get('u/{user}', 'UserController@show');
+Route::get('r/{sub}', 'SubController@show');
+//----------------------------------------//
+
+// Checking if the user is signed in (using AJAX)
+Route::get('authcheck', function () {
+    return json_encode(Auth::check());
 });
+//----------------------------------------//
+
+// Creating and storing a new sub
+Route::get('sub/new', [
+    'middleware' => 'auth',
+    'uses' => 'SubController@displayform'
+]);
+
+Route::post('sub/new', [
+    'middleware' => 'auth',
+    'uses' => 'SubController@storesub'
+]);
+//----------------------------------------//
+
+// Creating and storing a new post / link
+Route::get('post/new', [
+    'middleware' => 'auth',
+    'uses' => 'PostController@displayform',
+]);
+
+Route::post('post/new', [
+    'middleware' => 'auth',
+    'uses' => 'PostController@storepost'
+]);
+//----------------------------------------//
+
+// Voting on posts / links
+Route::post('vote', [
+    'middleware' => 'auth',
+    'uses' => 'VoteController@vote'
+]);
